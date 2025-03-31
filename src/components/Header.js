c8ae87f0ec67a1a1bc9c40e89f538100f01ebad0
Alexandra Somodi <asomodi@usc.edu>
2025-03-31 01:03:53 -0700

    landing page

---
+++ src/components/Header.js
@@ -0,0 +1,54 @@
+import React from "react";
+import { Link } from "gatsby";
+import "../styles/global.css";
+import "../styles/Header.css";
+import mtrCrest from "../images/mtr-crest-white.png";
+
+export const Header = () => {
+  return (
+    <header className="header">
+      {/* Left Section: Logo with a different background color */}
+
+        <div
+        >
+          <Link
+            to="/"
+            style={{
+              textDecoration: "none",
+              color: "#FFFFFF",
+            }}
+          >
+            <img src={mtrCrest} alt="MTR Logo" className="header-image"/>
+            <img src={mtrCrest} alt="MTR Crest" className="header-image-mobile"/>
+          </Link>
+        </div>
+
+      {/* Right Section: Navigation Menu */}
+      <div>
+        <nav>
+          <ul
+            className="nav-container"
+          >
+            {["home", "past shows", "board", "join"].map((item, index) => (
+              <li key={index}>
+                <Link
+                  to={`/${item.toLowerCase().replace(" ", "-")}`}
+
+                  className="nav-links"
+                  activeStyle={{
+                    fontWeight: "bold",
+                    textDecoration: "none",
+                  }}
+                >
+                  {item}
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </nav>
+      </div>
+    </header>
+  );
+};
+
+export default Header;

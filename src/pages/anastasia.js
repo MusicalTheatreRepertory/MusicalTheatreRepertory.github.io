61ce81e1ea4ea1b13308fef417d4e51892c218f7
Alexandra Somodi <asomodi@usc.edu>
2025-04-03 19:24:12 -0700

    update gray jacob bio

--- src/pages/anastasia.js
+++ src/pages/anastasia.js
@@ -115,7 +115,7 @@ const Anastasia = () => {
                 </Link>
             </div>
             <h1>Special Thanks</h1>
-            <p>We want to give special thanks for helping us make this project a reality to SDA, Edgard De Dios, Sophia Pollacchi, Grace Sutherland, Melina Limantzakis, MaryAnn Hu, Jenni Barber, Trojan Stunt Group, and the Scene Dock Theatre. </p>
+            <p>We want to give special thanks for helping us make this project a reality to SDA, Edgard De Dios, Sophia Pollacchi, Grace Sutherland, Melina Limantzakis, MaryAnn Hu, Jenni Barber, Rocco Turano, Trojan Stunt Group, and the Scene Dock Theatre. </p>
             <br />
             <h1>Donations</h1>
             <div className="button">
